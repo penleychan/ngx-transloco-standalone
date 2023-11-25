ed3583d64aba9f44a28a683de026c2758c27c8e6
Penley Chan <penleychan@gmail.com>
2023-11-25 15:31:48 -0700

    add standalone example

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@ngneat/transloco';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { LOCALIZE_ROUTER_CONFIG, LocalizeRouterModule, localizeRouterConfig } from '@penleychan/ngx-transloco-router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideRouter(routes),
+    importProvidersFrom(LocalizeRouterModule.forRoot(routes)),
+
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'fr'],
+        defaultLang: 'en',
+        fallbackLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader
+    }),
+    {
+      provide: LOCALIZE_ROUTER_CONFIG,
+      useValue: localizeRouterConfig({
+        translateRoute: true
+      })
+    }
+  ]
 };
